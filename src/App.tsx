18c7f878f7b093997fa35183fc7f6ecae0d20695
emiequine <emi.roberti@equineregister.co.uk>
2024-02-10 11:17:07 +0000

    tidy component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
-import React, { useState } from "react";
-import "./App.css";
-import Addresses from "./components/Addresses";
-import { LoadScript } from "@react-google-maps/api";
 import Map from "./components/Map";
 function App() {
-  //const [key] = useState(process.env.REACT_APP_G_API_KEY);
-
   return <Map />;
-  // return (
-  //   // <LoadScript googleMapsApiKey={key!} libraries={["places"]}>
-  //   //   <Addresses />
-  //     <Map />
-  //   {/* </LoadScript> */}
-  // );
 }
 
 export default App;
